76826e5faa3cc57a40f3a529242fd4fa17ee5293
Grant Poquiz <gpoquiz@gmail.com>
2023-08-11 11:59:15 -0500

    Filter trimming

--- src/_data/eleventyComputed.js
+++ src/_data/eleventyComputed.js
@@ -1,31 +1,36 @@
 function getFile(filePathStem) {
-  return filePathStem.match(/\/(.+)$/)[1];
+  return filePathStem.match(/\/([A-z]+)$/)[1];
 }
 
 function buildThumbnail(data) {
   const file = getFile(data.page.filePathStem);
-  return data.page.thumbnail ?? `/images/thumbnail/${file}.png`;
+  return data.page.thumbnail ?? `/images/thumbnails/${file}.png`;
 }
 
 function getParent(filePathStem) {
-  const results = filePathStem.match(/\/(.+)\/.+$/);
+  const results = filePathStem.match(/\/([A-z]+)\/[A-z]+$/);
   return results ? results[1] : "index";
 }
 
 function buildNavigation(data) {
-  const eleventyNavigation = {
-    key: data.page.fileslug,
-    parent: data.page.parent ?? getParent(data.page.filePathStem) ?? "index"
-  };
-
-  if (data.page.fileSlug === "") {
-    console.log(data);
+  if (!data.title)
+  {
+    return;
   }
+  const eleventyNavigation = {
+    key: data.page.fileSlug,
+    parent: data.parent ?? getParent(data.page.filePathStem) ?? "index"
+  };
 
   return eleventyNavigation;
 }
 
 function buildBackground(data) {
+  if (data.page.background)
+    return data.page.background;
+  if (!data.excerpt)
+    return  "/images/backgrounds/index.png";
+    
   const file = getFile(data.page.filePathStem);
   return data.page.background ?? `/images/backgrounds/${file}.png`;
 }
